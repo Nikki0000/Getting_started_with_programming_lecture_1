e65a1b73455cfc41b1c7e270ded44390095c766a
Savodyarkin Nikita <snikitafut@mail.ru>
2023-11-15 17:31:40 +0300

    Example_003 Sum

---
+++ Example003_Sum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example003_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 8;
+            int numberB = 8;
+            int result = numberA + numberB;
+            Console.WriteLine(result);
+        }
+    }
+}
