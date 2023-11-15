7deccdcc9a4982da7e5b4e9654ea003d2db4eef7
Savodyarkin Nikita <snikitafut@mail.ru>
2023-11-15 18:00:12 +0300

    Add random function

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -6,8 +6,11 @@ namespace Example003_Sum
     {
         static void Main(string[] args)
         {
-            int numberA = 8;
-            int numberB = 8;
+
+            int numberA = new Random().Next(1, 10);
+            Console.WriteLine(numberA);
+            int numberB = new Random().Next(1, 10);
+            Console.WriteLine(numberB);
             int result = numberA + numberB;
             Console.WriteLine(result);
         }
